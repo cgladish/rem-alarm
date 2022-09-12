67f24a63a34463085a53dc94de406e4627ff81e0
Chase Gladish <chase.gladish@gmail.com>
2022-09-11 19:42:50 -0700

    Scrollable

--- views/Alarms/index.tsx
+++ views/Alarms/index.tsx
@@ -1,4 +1,11 @@
-import { StyleSheet, Switch, Text, TouchableOpacity, View } from "react-native";
+import {
+  ScrollView,
+  StyleSheet,
+  Switch,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
 import { useDispatch, useSelector } from "react-redux";
 import Ionicons from "@expo/vector-icons/Ionicons";
 import { Dispatch } from "../../store";
@@ -12,10 +19,10 @@ const padTimeString = (time: number): string =>
 const dayOfWeekToText = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
 
 const AlarmItem = ({
-  key,
+  index,
   alarmInfo,
 }: {
-  key: number;
+  index: number;
   alarmInfo: AlarmInfo;
 }) => {
   const [isExpanded, setIsExpanded] = useState(false);
@@ -74,7 +81,7 @@ const AlarmItem = ({
               dispatch({
                 type: "@@alarms/UPDATE",
                 payload: {
-                  index: key,
+                  index,
                   data: { ...alarmInfo, enabled: !alarmInfo.enabled },
                 },
               });
@@ -88,6 +95,7 @@ const AlarmItem = ({
           />
         </View>
       </View>
+      {isExpanded && <View></View>}
     </TouchableOpacity>
   );
 };
@@ -97,54 +105,67 @@ export default function Alarms() {
   const dispatch = useDispatch<Dispatch>();
 
   return (
-    <View
-      style={{
-        flex: 1,
-        paddingHorizontal: 15,
-        backgroundColor: "#111",
-        alignItems: "center",
-        justifyContent: "center",
-      }}
-    >
-      {alarms.map((alarmInfo, index) => (
-        <AlarmItem key={index} alarmInfo={alarmInfo} />
-      ))}
-      <TouchableOpacity
+    <>
+      <ScrollView style={{ width: "100%" }}>
+        <View
+          style={{
+            flex: 1,
+            paddingHorizontal: 15,
+            backgroundColor: "#111",
+            alignItems: "center",
+            justifyContent: "center",
+            paddingBottom: 150,
+          }}
+        >
+          {alarms.map((alarmInfo, index) => (
+            <AlarmItem key={index} index={index} alarmInfo={alarmInfo} />
+          ))}
+        </View>
+      </ScrollView>
+      <View
         style={{
           justifyContent: "center",
           alignItems: "center",
-          borderRadius: 100,
-          backgroundColor: "#222",
-          width: 100,
-          height: 100,
           position: "absolute",
           bottom: 40,
+          width: "100%",
         }}
-        onPress={() =>
-          dispatch({
-            type: "@@alarms/ADD",
-            payload: {
-              data: {
-                time: { hours: 0, minutes: 0 },
-                repeat: {
-                  0: false,
-                  1: false,
-                  2: false,
-                  3: false,
-                  4: false,
-                  5: false,
-                  6: false,
-                },
-                enabled: true,
-              },
-            },
-          })
-        }
       >
-        <Text style={{ fontFamily: "Roboto", fontSize: 48, color: "#fff" }}>
-          +
-        </Text>
-      </TouchableOpacity>
-    </View>
+        <TouchableOpacity
+          style={{
+            justifyContent: "center",
+            alignItems: "center",
+            borderRadius: 100,
+            backgroundColor: "#444",
+            width: 100,
+            height: 100,
+          }}
+          onPress={() =>
+            dispatch({
+              type: "@@alarms/ADD",
+              payload: {
+                data: {
+                  time: { hours: 0, minutes: 0 },
+                  repeat: {
+                    0: false,
+                    1: false,
+                    2: false,
+                    3: false,
+                    4: false,
+                    5: false,
+                    6: false,
+                  },
+                  enabled: true,
+                },
+              },
+            })
+          }
+        >
+          <Text style={{ fontFamily: "Roboto", fontSize: 48, color: "#fff" }}>
+            +
+          </Text>
+        </TouchableOpacity>
+      </View>
+    </>
   );
 }
